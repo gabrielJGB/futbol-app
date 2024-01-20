f170fc692b9ae1975d11347b9e120673f7de21dd
gabriel <brunengo.gabriel@gmail.com>
2024-01-20 19:17:11 -0300

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
           <>
             <Navigation />
             <Config />
-            <LeaguesNames />
+            {/* <LeaguesNames /> */}
 
             {
               data.loading ?
